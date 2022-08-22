1acaa3f24bc12d755bee1a57a78afbad96ae5537
Mapc1 <marcocosta579@gmail.com>
2022-08-22 18:41:26 +0100

    Set ammo count label when item is not a gun

--- Assets/Scripts/TestRogueLike/UI/AmmoCount.cs
+++ Assets/Scripts/TestRogueLike/UI/AmmoCount.cs
@@ -19,6 +19,8 @@ namespace TestRogueLike.UI
             var item = playerWorld._player._inventory.GetActiveItem();
             if (item is Gun gun)
                 _label.SetText($"{gun.bulletsRemaining}/{gun.magSize}");
+            else
+                _label.SetText("");
         }
     }
 }
